42b32df3485e79357fa4fbc4f8ae029e23b805ee
Knurobroddy <macadamczewski@gmail.com>
2022-05-03 22:37:18 +0200

    Tab update and url queries.

--- background.js
+++ background.js
@@ -1,1 +1,15 @@
-console.log("lol");
+function scanTabs() {
+    chrome.tabs.query({ //This method output active URL 
+        "active": true,
+        "currentWindow": true,
+        "status": "complete",
+        "windowType": "normal"
+    }, function (tabs) {
+        for (tab in tabs) {
+            console.log(tabs[tab].url);
+        }
+    });
+}
+
+chrome.tabs.onActivated.addListener(scanTabs);
+chrome.tabs.onUpdated.addListener(scanTabs);
