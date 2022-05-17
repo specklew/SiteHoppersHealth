862b6e3df539958795facd632d954641e153fd21
Knurobroddy <macadamczewski@gmail.com>
2022-05-17 19:35:43 +0200

    Pet image double check.

--- popup.js
+++ popup.js
@@ -8,6 +8,7 @@ setTimeout(function(){
     chrome.storage.sync.get(["hp"], function(items){
         hpbar.set(items.hp, true);
         let imageNum = Math.ceil(10 - (items.hp - 1)/10);
+        if(imageNum >= 11) imageNum = 10;
         if(imageNum <= 0) imageNum = 1;
         let imageSrc = "images/turtle_example/t" + imageNum + ".png";
         document.getElementById("turtle").src = imageSrc;
